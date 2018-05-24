a326b9e570c537a339c91e9b6f68060dc83bda37
Tanaka Chingonzo <tchingonzo21@wooster.edu>
2018-05-24 00:46:22 -0700

    all commits

---
+++ example/db.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+  return {
+    profiles: require('./profiles.json'),
+    tweets: require('./tweets.json'),
+    hashtag_cats_are_evil: require('./hashtag_cats_are_evil.json'),
+    hashtag_hangry: require('./hashtag_hangry.json')
+  }
+}
